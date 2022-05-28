4b245c1d4fb83af1ca9a98f61df61beecb7785b8
WARRIORS <56763972+ccadmin1@users.noreply.github.com>
2022-05-28 13:35:22 +0530

    Initial commit

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/ccadmin1/EVaN.git /EVaN
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /EVaN
+fi
+cd /EvaMaria
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
